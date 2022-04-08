bc76dadcaf967e11b9cfd6a2f6d12207a4d32edd
mtorres <tcontreras993@gmail.com>
2022-04-08 15:33:37 -0300

    modificacion en productController.js para que funcionen los styles

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,7 +3,6 @@ const views = path.join(__dirname + "/../views");
 const testData = require("../models/data");
 
 const productController = {
-<<<<<<< HEAD
   details: (req, res) => {
     res.render(views + "/products/productDetail.ejs", {
       css: "ProDeta",
@@ -16,20 +15,6 @@ const productController = {
       title: "Store - DHaunters",
     });
   },
-=======
-	details: (req, res) => {
-		res.render(views + "/products/productDetail.ejs", {
-			css: "ProDelta",
-			title: "Detalles del Producto - DHaunters",
-		});
-	},
-	store: (req, res) => {
-		res.render(views + "/products/marketplace.ejs", {
-			css: "",
-			title: "Store - DHaunters",
-		});
-	},
->>>>>>> cab6865fc2e6f3c2bab91fa6fd16a23171494410
 };
 
 module.exports = productController;
