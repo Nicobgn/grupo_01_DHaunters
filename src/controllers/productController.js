7b248a9425d97f16eab4acdc13613791269634e2
Niicoojk <nicobarragan_@hotmail.com>
2022-04-08 01:17:26 -0300

    Añadiendo el carrito, agregando carpeta models y algo de información en ella

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,18 +1,20 @@
 const path = require("path");
 const views = path.join(__dirname + "/../views");
+const testData = require("../models/data");
+
 const productController = {
-  details: (req, res) => {
-    res.render(views + "/products/productDetail.ejs", {
-      css: "ProDelta",
-      title: "Detalles del Producto - DHaunters",
-    });
-  },
-  store: (req, res) => {
-    res.render(views + "/products/marketplace.ejs", {
-      css: "",
-      title: "Store - DHaunters",
-    });
-  },
+	details: (req, res) => {
+		res.render(views + "/products/productDetail.ejs", {
+			css: "ProDelta",
+			title: "Detalles del Producto - DHaunters",
+		});
+	},
+	store: (req, res) => {
+		res.render(views + "/products/marketplace.ejs", {
+			css: "",
+			title: "Store - DHaunters",
+		});
+	},
 };
 
 module.exports = productController;
--- src/routes/main.js
+++ src/routes/main.js
@@ -5,6 +5,5 @@ const { home, error404 } = require("../controllers/mainController");
 const router = express.Router();
 
 router.get("/", home);
-router.get("/404", error404);
 
 module.exports = router;
