aa84b029f8db1be061b4268c958b1411f5ef3a99
HawkDiego <giraldito99@gmail.com>
2022-07-25 19:34:08 -0500

    update login validation

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,29 +1,58 @@
-window.onload = function () {
-  const formulario = document.querySelector("form");
-  const password = document.querySelector("#password");
-  const datoIngreso = document.querySelector("#dato_ingreso");
-  const errores = document.querySelector(".error-msg");
-  function stopSubmit(e) {
-    let errors = [];
+const formulario = document.querySelector("form");
+const inputs = document.querySelectorAll("form input");
 
-    if (datoIngreso.value == "") {
-      errors.push("El nombre de usuario es obligatorio");
-    }
-    if (password.value == "") {
-      errors.push("El campo de contraseña debe estar completo");
-    }
-
-    if (errors.length > 0) {
-      e.preventDefault();
-      errores.classList.add("error-msg");
-      for (const erorr of errors) {
-        errores.innerHTML += `<li> ${erorr} </li>`;
-      }
-
-      /* alert("no enviar"); */
-      console.log("no se envio el formulario");
-    }
-  }
-
-  formulario.addEventListener("submit", stopSubmit);
+const expresiones = {
+  usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  password: /^.{4,12}$/, // 4 a 12 digitos.
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  telefono: /^\d{7,14}$/, // 7 a 14 numeros.
 };
+
+var campos = {
+  dato_ingreso: false,
+  password: false,
+};
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "dato_ingreso":
+      if (
+        expresiones.usuario.test(e.target.value) ||
+        expresiones.correo.test(e.target.value)
+      ) {
+        document.querySelector("#dato_ingreso").classList.remove("drawer-red");
+        document.querySelector("#dato_ingreso").classList.add("drawer");
+        document.querySelector(".error-msg").classList.add("oculto");
+        campos.dato_ingreso = true;
+      } else {
+        document.querySelector("#dato_ingreso").classList.remove("drawer");
+        document.querySelector("#dato_ingreso").classList.add("drawer-red");
+        document.querySelector(".error-msg").classList.remove("oculto");
+        campos.dato_ingreso = false;
+      }
+      break;
+    case "password":
+      if (e.target.value) {
+        campos.password = true;
+      } else {
+        campos.password = false;
+      }
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (campos.dato_ingreso && campos.password) {
+    document.querySelector("#error-msg").classList.add("oculto");
+    formulario.submit();
+  } else {
+    document.querySelector("#error-msg").classList.remove("oculto");
+  }
+});
+
