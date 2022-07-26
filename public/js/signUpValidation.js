5ebc704dcccba50bbbacdcdc999500009fd58daa
HawkDiego <giraldito99@gmail.com>
2022-07-25 22:49:46 -0500

    login validations

--- public/js/signUpValidation.js
+++ public/js/signUpValidation.js
@@ -1,51 +1,104 @@
-window.onload = function () {
-  const image = document.querySelector("#image");
-  const firstName = document.querySelector("#first_name");
-  const lastName = document.querySelector("#last_name");
-  const userName = document.querySelector("#user_name");
-  const email = document.querySelector("#email");
-  const adress = document.querySelector("#adress");
-  const password = document.querySelector("#password");
-  const checkPassword = document.querySelector("#checkPassword");
-  const formulario = document.querySelector("form");
-  const errores = document.querySelector(".error-msg");
+const formulario = document.querySelector("form");
+const inputs = document.querySelectorAll("form input");
 
-  function stopSubmit(e) {
-    let errors = [];
-
-    if (firstName.value == "") {
-      errors.push("debe ingresar un nombre");
-    }
-    if (lastName.value == "") {
-      errors.push("Debe ingresar un apellido valido");
-    }
-    if (userName == "") {
-      errors.push("Debe ingresar un nombre de usuario valido");
-    }
-    if (email == "") {
-      errors.push("Debe ingresar un mail valido");
-    }
-    if (adress.value == "") {
-      errors.push("debe ingresarce una direccion valida");
-    }
-    if (password.value == "") {
-      errors.push("Debe ingresar una contraseña");
-    }
-    if (checkPassword == "") {
-      errors.push("Debe ingresar la misma contraseña");
-    }
-
-    if (errors.length > 0) {
-      e.preventDefault();
-      errores.classList.add("error-msg");
-      for (const erorr of errors) {
-        errores.innerHTML += `<li> ${erorr} </li>`;
-      }
-
-      /* alert("no enviar"); */
-      console.log("no se envio el formulario");
-    }
-  }
-
-  formulario.addEventListener("submit", stopSubmit);
+const expresiones = {
+  usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+  nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  password: /^.{8,16}$/, // 4 a 12 digitos.
+  correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+  direccion: /^[a-zA-Z0-9\_\-\s\#]{4,40}$/,
 };
+
+let campos = {
+  first_name: false,
+  last_name: false,
+  user_name: false,
+  email: false,
+  adress: false,
+  password: false,
+};
+
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "first_name":
+      validarCampo(expresiones.nombre, e.target, "first_name");
+      break;
+    case "last_name":
+      validarCampo(expresiones.nombre, e.target, "last_name");
+      break;
+    case "user_name":
+      validarCampo(expresiones.nombre, e.target, "user_name");
+      break;
+    case "email":
+      validarCampo(expresiones.correo, e.target, "email");
+      break;
+    case "adress":
+      validarCampo(expresiones.direccion, e.target, "adress");
+      break;
+    case "password":
+      validarCampo(expresiones.password, e.target, "password");
+      validarPassword2();
+      break;
+    case "checkPassword":
+      validarPassword2();
+      break;
+  }
+};
+
+const validarCampo = (valTyp, valueInput, campo) => {
+  if (valTyp.test(valueInput.value)) {
+    document.querySelector(`#${campo}`).classList.remove("drawer-red");
+    document.querySelector(`#${campo}`).classList.add("drawer");
+    document.querySelector(`form div .${campo}`).classList.add("oculto");
+    campos[campo] = true;
+  } else {
+    document.querySelector(`#${campo}`).classList.remove("drawer");
+    document.querySelector(`#${campo}`).classList.add("drawer-red");
+    document.querySelector(`form div .${campo}`).classList.remove("oculto");
+    campos[campo] = false;
+  }
+};
+
+const validarPassword2 = () => {
+  const inputPassword = document.getElementById("password");
+  const inputCheckPassword = document.getElementById("checkPassword");
+
+  if (inputPassword.value !== inputCheckPassword.value) {
+    document.getElementById(`checkPassword`).classList.add("drawer-red");
+    document.getElementById(`checkPassword`).classList.remove("drawer");
+    document
+      .querySelector(`form div .checkPassword`)
+      .classList.remove("oculto");
+    campos.password = false;
+  } else {
+    document.getElementById(`checkPassword`).classList.remove("drawer-red");
+    document.getElementById(`checkPassword`).classList.add("drawer");
+    document.querySelector(`form div .checkPassword`).classList.add("oculto");
+    campos.password = true;
+  }
+};
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+});
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let terminos = document.querySelector("#tyc");
+  if (
+    campos.user_name &&
+    campos.last_name &&
+    campos.first_name &&
+    campos.email &&
+    campos.password &&
+    campos.adress &&
+    terminos.checked
+  ) {
+    document.querySelector("#error-msg").classList.add("oculto");
+    formulario.submit();
+  } else {
+    document.querySelector("#error-msg").classList.remove("oculto");
+  }
+});
