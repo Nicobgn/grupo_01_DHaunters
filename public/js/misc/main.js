8cf50fd4fd75b073b67182fdf7ad879bd6b09d15
Niicoojk <nicobarragan_@hotmail.com>
2022-07-28 02:46:51 -0300

    agregando el close

--- public/js/misc/main.js
+++ public/js/misc/main.js
@@ -1,26 +1,26 @@
-const burger = document.getElementById('burgerMenu');
-const mobileNav = document.getElementsByClassName('mobileNav');
-const popup = document.getElementById("popup")
-const overlay = document.getElementById("overlay")
-const closePopup = document.getElementById("closePopup")
+const burger = document.getElementById("burgerMenu");
+const mobileNav = document.getElementsByClassName("mobileNav");
+const popup = document.getElementById("popup");
+const overlay = document.getElementById("overlay");
+const closePopup = document.getElementById("closePopup");
 const openPopup = document.getElementById("choiceLogOrSign");
 const checkoutButton = document.getElementById("checkoutButton");
 
-burger.addEventListener('click',(e)=>{
-    e.preventDefault();
-    mobileNav[0].classList.toggle('active');
-    checkoutButton.classList.toggle('toggle')
+burger.addEventListener("click", (e) => {
+  e.preventDefault();
+  mobileNav[0].classList.toggle("active");
+  checkoutButton.classList.toggle("toggle");
 });
 
-openPopup.addEventListener('click',(e)=>{
-    e.preventDefault();
-    overlay.classList.add('active');
-    popup.classList.add('active');
-    checkoutButton.classList.add('toggle')
-})
-closePopup.addEventListener('click',(e)=>{
-    e.preventDefault();
-    overlay.classList.remove('active')
-    popup.classList.remove('active')
-    checkoutButton.classList.remove('toggle')
-})
+openPopup.addEventListener("click", (e) => {
+  e.preventDefault();
+  overlay.classList.add("active");
+  popup.classList.add("active");
+  checkoutButton.classList.add("toggle");
+});
+closePopup.addEventListener("click", (e) => {
+  e.preventDefault();
+  overlay.classList.remove("active");
+  popup.classList.remove("active");
+  checkoutButton.classList.remove("toggle");
+});
