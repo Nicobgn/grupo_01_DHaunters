19dab2b7e8ae54d07eea76a006b7d6757f30005a
HawkDiego <giraldito99@gmail.com>
2022-07-28 16:51:26 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const methodOverride = require("method-override");
 const session = require("express-session");
 const cookies = require("cookie-parser");
+const cors = require("cors");
 
 // Requiring Middlewares
 const cookieLogged = require("./middlewares/other/cookieLogged");
@@ -31,6 +32,7 @@ app.use(methodOverride("_method"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
 
 // Express Session && Cookies
 let ss = 1000;
